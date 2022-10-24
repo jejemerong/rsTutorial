4654458be6a4dfab85138338838b7c21fa021a44
jejemerong <jemerald@jejemerongui-MacBookPro.local>
2022-10-24 12:56:26 +0900

    boolean and char type

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -6,11 +6,21 @@ fn main() {
     // println!("The value of x is: {x}");
 
     // shadowing 과 mut 의 차이점! => 
-    let x = 5;
-    let x = x + 1;
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
-    println!("The value of x is {x}");
+    // let x = 5;
+    // let x = x + 1;
+    // {
+    //     let x = x * 2;
+    //     println!("The value of x in the inner scope is: {x}");
+    // }
+    // println!("The value of x is {x}");
+
+    // TODO: t와 f 에 _ 붙이는 이유!
+    // if this is intentional, prefix it with an underscore: `_f`
+    let _t:bool = true;
+    let _f: bool = false;
+
+    // 문자 타입
+    let c = 'z';
+    let z = 'z';
+    let heart_person = '🥰';
 }
