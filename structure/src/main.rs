74cede34d24f88748286ca786103877ff0678d51
jejemerong <9708jjw@gmail.com>
2022-11-15 23:21:36 +0900

    impl 함수 내 self 인자를 받는 함수 추가

--- structure/src/main.rs
+++ structure/src/main.rs
@@ -11,3 +11,26 @@ fn main() {
 fn area(length: u32, width: u32) -> u32 {
     length * width
 }
+
+#![allow(unused)]
+fn main() {
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+/**
+    method 
+    impl 안에는 self 인자를 받는 함수를 따로 정의할 수 있다. 
+*/
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.length * self.width
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+}
+}
